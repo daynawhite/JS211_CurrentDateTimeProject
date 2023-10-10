9744d26a84d1a655d5a151b9271757b1c2690d31
daynawhite <dayna26.2@gmail.com>
2023-10-09 19:37:04 -0500

    js code additions

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ const displayDate = () => {
 
 
 // Write a JavaScript program to convert a string to the number.
-let nbr = Number('765')
-console.log(nbr)
+// let nbr = Number('765')
+// console.log(nbr)
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -34,8 +34,58 @@ console.log(nbr)
   // * NaN
   // * String
 
-  
-  
+
+
+let itemInput
+const formOne = document.querySelector('#formOne')
+const formOneInput = document.querySelector('#dataItem')
+
+formOneInput.addEventListener("submit", function(event){
+event.preventDefault()
+console.log(formOneInput.value)
+})
+
+// const saveItem = (item) => {
+//   itemInput = item.value
+//   console.log(itemInput)
+// }
+
+// function itemID(valueX) {
+// if (valueX === 'true' || valueX === 'false') {
+//   console.log("Boolean")
+// } else if (valueX === 'null') {
+//   console.log("Null") 
+// } else if (!valueX.length) {
+//  console.log("Undefined")
+// }else if (!isNaN(Number(valueX))) {
+//   console.log("Number")
+// } else if (isNaN(Number(valueX))) {
+//   console.log("NaN")
+// } else console.log('String')
+// }
+
+// itemID(itemInput)
+
+// function getInputType(value) {
+//   console.log(value)
+//   if (value === "")
+//       {return "";
+//     console.log();}
+//   else if(value === 'true' || value === "false")
+//       {return "boolean";
+//       console.log(value);}
+//   else if(!isNaN(Number(value)))
+//       return "number";
+//   else
+//       return typeof value;
+// }
+
+
+// getInputType(true)
+
+
+// let x = 7
+// console.log(typeof x)
 
   
 // Write a JavaScript program that adds 2 numbers together.
@@ -44,9 +94,31 @@ console.log(nbr)
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+// const firstItem = (x) => {
+//   if ((x === 'undefined')
+//       || (x === '')
+//       || (x === 'false')
+//       || (x === '0')
+//       || (x === 'zero')
+//       || (x === 'null')){
+//     return false;
+//   // console.log(`${x} is a falsy value.`);
+//   }
+// //   else {
+// //   return true;
+
+// // }
+
+// console.log(dType('undefined'));
+
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+
+
+// const compareItems() {
+//   if firstItem() && secondItem()
+// }
 
 
 
